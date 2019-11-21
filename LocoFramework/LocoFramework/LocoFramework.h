ea8b54eedadf5321b8e718ccd91908b631b5e64c
Prasad Nesargi <prasadnesargi9@gmail.com>
2019-11-21 11:59:23 +0530

    Finished our LocoFramework

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by Prasad Nesargi on 21/11/19.
+//  Copyright © 2019 MinersINC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Prasad Nesargi on 21/11/19.
+//  Copyright © 2019 MinersINC. All rights reserved.
+//
+
+import Foundation
+
+private class Service {
+    
+    private init() {}
+     
+    public static func doSomething() -> String {
+        return "Did some stuff, here ypu go homeboi!"
+    }
+}
